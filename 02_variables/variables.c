19842eb2ae472048f3bde9df5c61d6303583b5ec
kiernan0676 <klauderdale0676@floridapoly.edu>
2025-08-25 20:20:26 +0000

    added student ID board

---
+++ 02_variables/variables.c
@@ -0,0 +1,27 @@
+/*
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+// Create a variable
+int first_number;
+// Assign a value to a variable
+first_number = 5;
+
+// Create and initialize
+int second_number = 78;
+
+// Print the value of a variable
+printf("The first number is %d\n", first_number); // %d - conversion specifier
+
+printf("The second number is %d\n", second_number);
+
+// calculate the sum
+int sum = first_number + second_number;
+printf("%d + %d = %d\n", first_number, second_number, sum);
+
+
+    return 0;
+}
